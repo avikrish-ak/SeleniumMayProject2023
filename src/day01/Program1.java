58b136a0baf213f30e3b2d0c491a28f889161eb9
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-05-24 20:34:03 +0530

    day01

--- src/day01/Program1.java
+++ src/day01/Program1.java
@@ -1,10 +1,16 @@
 package day01;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Program1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver;
+		driver = new FirefoxDriver();
+		driver.get("https://www.selenium.dev/");
 	}
 
 }
